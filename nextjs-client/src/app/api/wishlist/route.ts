df6cb8ed3840d3434465f2c6e3264465a70ddfb8
RenFrost <diidnugroho@gmail.com>
2024-10-23 20:27:19 +0700

    feat: adding add product to wishlist and get user wishlist functionality

--- nextjs-client/src/app/api/wishlist/route.ts
+++ nextjs-client/src/app/api/wishlist/route.ts
@@ -1,14 +1,43 @@
-import { errorHandler } from "@/helpers/errorHandler"
+import Wishlist from "@/db/models/Wishlist";
+import { errorHandler } from "@/helpers/errorHandler";
 
 export async function POST(request: Request) {
+  try {
+    const userId = request.headers.get("x-user-id");
+
+    const body = await request.json();
+
+    const { productId }: { productId: string | undefined } = body;
+
+    if (!userId || !productId)
+      throw { message: "product or user id is required" };
+
+    const newWishlist = {
+      userId: userId,
+      productId: productId,
+    };
+
+    await Wishlist.addWishlist(newWishlist);
+
+    return Response.json({ message: "ok" });
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      return errorHandler({ message: "product or user id is required" });
+    }
+    return errorHandler(error);
+  }
+}
+
+export async function GET(request: Request) {
     try {
-        const userId = request.headers.get("x-user-id")
-        const { productId } = await request.json()
-        const newWishlist = {
-            userId: userId,
-            productId: productId
-        }
-        return Response.json({message: "ok"})
+        const userId = request.headers.get("x-user-id");
+
+        if(!userId) throw {message: "userId is required", status: 400 }
+        
+        const wishlist = await Wishlist.getUserWishlist(userId)
+        
+
+        return Response.json(wishlist)
     } catch (error) {
         return errorHandler(error)
     }
--- nextjs-client/src/db/models/Wishlist.ts
+++ nextjs-client/src/db/models/Wishlist.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { database } from "../config/mongodb"
 
 class Wishlist {
@@ -5,10 +6,24 @@ class Wishlist {
         return database.collection("wishlist")
    }
 
-   static async addWishlist(newWishlist: {userId: string, productId: string}) {
+   static async addWishlist(payload: {userId: string, productId: string}) {
+        const newWishlist = {
+            userId: new ObjectId(payload.userId),
+            productId: new ObjectId(payload.productId)
+        }
+
         const result = await this.collection().insertOne(newWishlist)
 
         return result;
+   }
+
+   static async getUserWishlist(userId: string) {
+   
+        const wishlist = await this.collection().find({
+            userId: new ObjectId(userId)
+        }).toArray()
+
+        return wishlist
    }
 }
 
