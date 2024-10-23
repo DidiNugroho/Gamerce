2d3ab2662b4995fd04185a99aa7ad69718187dab
RenFrost <diidnugroho@gmail.com>
2024-10-23 15:05:58 +0700

    feat: completed register functionality

--- nextjs-client/src/app/api/products/[slug]/route.ts
+++ nextjs-client/src/app/api/products/[slug]/route.ts
@@ -5,16 +5,19 @@ export async function GET(
   { params }: { params: { slug: string } }
 ) {
   try {
-      const { slug } = params;
-      const product = await Product.getBySlug(slug);
-      return Response.json(product);
+    const { slug } = params;
+    const product = await Product.getBySlug(slug);
+    return Response.json(product);
   } catch (error: any) {
-    const message = error.message || 'Internal Server Error'
-    const status = error.status || 500
-    return Response.json({
-        message: message
-    }, {
+    const message = error.message || "Internal Server Error";
+    const status = error.status || 500;
+    return Response.json(
+      {
+        message: message,
+      },
+      {
         status: status,
-    })
+      }
+    );
   }
 }
---
+++ nextjs-client/src/db/models/User.ts
@@ -0,0 +1,36 @@
+import { hashPassword } from "@/helpers/jwt";
+import { database } from "../config/mongodb";
+import { z } from "zod";
+
+const UserSchema = z.object({
+  name: z.string({ message: "Name Required" }),
+  username: z.string({ message: "Username Required" }),
+  email: z.string().email({ message: "Invalid Email Address" }),
+  password: z
+    .string({ message: "Password Required" })
+    .min(5, { message: "Password length must be more than 4" }),
+});
+
+type NewUser = z.infer<typeof UserSchema>;
+
+class User {
+  static collection() {
+    return database.collection<NewUser>("users");
+  }
+
+  static async register(user: NewUser) {
+    UserSchema.parse(user);
+
+    const existingUser = await this.collection().findOne({
+      $or: [{ username: user.username }, { email: user.email }],
+    });
+
+    if(existingUser) throw {message: "Email/Username already exist", status: 400};
+
+    user.password = hashPassword(user.password)
+
+    return this.collection().insertOne(user);
+  }
+}
+
+export default User;
