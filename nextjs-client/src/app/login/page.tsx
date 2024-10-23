dfda89244eb008dbc24b691ab4d6185d54cb6454
RenFrost <diidnugroho@gmail.com>
2024-10-23 17:24:13 +0700

    feat: added login functionality to login page

--- nextjs-client/src/app/login/page.tsx
+++ nextjs-client/src/app/login/page.tsx
@@ -1,8 +1,55 @@
+"use client"
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
+import Swal from "sweetalert2";
 
 export default function Login() {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        body: JSON.stringify(input),
+      });
+      const response = await res.json();
+
+      if (!res.ok) {
+        throw response;
+      }
+
+      router.push("/");
+    } catch (error: any) {
+      Swal.fire({
+        title: "Error!",
+        text: error.message,
+        icon: "error",
+        confirmButtonText: "Cool",
+      });
+    }
+  };
+
   return (
-    <form className="flex flex-col justify-center max-w-lg mx-auto px-4 mt-8 space-y-6 font-[sans-serif] text-[#333]">
+    <form
+      className="flex flex-col justify-center max-w-lg mx-auto px-4 mt-8 space-y-6 font-[sans-serif] text-[#333]"
+      onSubmit={handleSubmit}
+    >
       <div>
         <label className="mb-2 text-2xl block font-extrabold">
           Welcome to Gamerce
@@ -13,11 +60,17 @@ export default function Login() {
         <input
           type="text"
           placeholder="Email"
+          name="email"
+          value={input.email}
+          onChange={handleChange}
           className="px-4 py-3 mt-6 text-lg rounded-md bg-white border border-gray-400 w-full outline-blue-500"
         />
         <input
           type="password"
           placeholder="Password"
+          name="password"
+          value={input.password}
+          onChange={handleChange}
           className="px-4 mt-6 py-3 text-lg rounded-md bg-white border border-gray-400 w-full outline-blue-500"
         />
       </div>
